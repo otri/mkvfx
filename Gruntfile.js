81b910b9eae5e8291edc9f37f7ea179e2399950d
Aaron Hilton <aaron@conquermobile.com>
2015-02-25 13:12:09 -0800

    Made code compliant with the JSLint coming with node.js these days, and added missing sleep dependency.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,9 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    nodeunit: {
-      files: ['test/**/*_test.js'],
-    },
+    // nodeunit: {
+    //   files: ['test/**/*_test.js'],
+    // },
     jshint: {
       options: {
         jshintrc: '.jshintrc'
@@ -17,9 +17,9 @@ module.exports = function(grunt) {
       lib: {
         src: ['lib/**/*.js']
       },
-      test: {
-        src: ['test/**/*.js']
-      },
+      // test: {
+      //   src: ['test/**/*.js']
+      // },
     },
     watch: {
       gruntfile: {
@@ -30,19 +30,19 @@ module.exports = function(grunt) {
         files: '<%= jshint.lib.src %>',
         tasks: ['jshint:lib', 'nodeunit']
       },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      },
+      // test: {
+      //   files: '<%= jshint.test.src %>',
+      //   tasks: ['jshint:test', 'nodeunit']
+      // },
     },
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  // grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+  grunt.registerTask('default', ['jshint'/*, 'nodeunit'*/]);
 
 };
